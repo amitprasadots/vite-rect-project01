37be72ead04310b5b704b40bb562a687d8bcf240
Amit <amitprasad@learningspiral.co.in>
2023-09-26 14:20:50 +0530

    newhasd

---
+++ src/components/Newbranchtest.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Newbranchtest = () => {
+    return (
+        <div>
+            <h1>This is  New branch test::</h1>
+        </div>
+    )
+}
+
+export default Newbranchtest
