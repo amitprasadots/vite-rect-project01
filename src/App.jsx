45031ec6536fb3948c8c5b47950399d3319a6dca
Amit <amitprasad@learningspiral.co.in>
2023-09-26 14:10:16 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import Change from "./components/change"
+
 function App() {
 
   return (
     <>
       <h1>let change react::</h1>
+      <Change/>
     </>
   )
 }
---
+++ src/components/Change.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Change = () => {
+    return (
+        <div>
+          <h1>Chnage Components Used !!</h1>  
+        </div>
+    )
+}
+
+export default Change
